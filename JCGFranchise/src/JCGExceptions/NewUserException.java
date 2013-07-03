f7ecb2cfac6a591c989d91fe3558cbfb79c9bd1d
Taylor <taylor@taylor-PC.gateway.2wire.net>
2013-07-02 22:36:11 -0400

    new user exception

---
+++ JCGFranchise/src/JCGExceptions/NewUserException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JCGExceptions;
+
+/**
+ *
+ * @author taylor
+ */
+public class NewUserException extends Exception{
+    public NewUserException(String message) {
+        super(message);
+    }
+}
