0cd6e1e952c644d00845e354b6abf5a2936fa58f
Taylor <taylor@taylor-PC.gateway.2wire.net>
2013-07-04 19:02:57 -0400

    class to hold strings for queries

---
+++ JCGFranchise/src/db/QStrings.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+/**
+ *
+ * @author taylor
+ */
+final class QStrings {
+    
+    // Fracnhise
+    public String update_fran = "UPDATE Franchise SET Address = ?,"
+                    + " City = ?, State = ?, Zip = ?, Phone = ?, Email = ?, "
+                    + " AirportID = ? WHERE FranchiseNumber = ?";
+    public String insert_fran = "INSERT INTO Franchise (Address, City,"
+                    + " State, Zip, Phone, Email, AirportID) values "
+                    + "(?, ?, ?, ?, ?, ?, ?)";
+    
+    // Vehicle
+    public String insert_vehicle = "INSERT INTO Vehicle (VehicleID, VIN, Make "
+             + "Model, Year, Millage, Capacity, VCondition, Tablet, RentalPrice"
+             + " FranchiceNumber VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+     
+    public String update_vehicle = "UPDATE Vehicle SET Millage = ?, VCondition "
+             + "= ?, Tablet = ?, RentalPrice = ?, FranchiseNumber = ? WHERE "
+             + "VehicleID = ?";
+    
+    // Maintenance
+    public String insert_maintenance = "INSERT INTO Maintenance(ServiceNumber, "
+            + "VehicleID, ServiceType, ServiceCost) VALUES(?,?,?,?)";
+}
