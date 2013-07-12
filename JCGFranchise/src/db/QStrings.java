19fbcc008ee8053d8fc52ecef87e6326afafec74
Taylor <treighard80@gmail.com>
2013-07-11 23:11:54 -0400

    added customer strings

--- JCGFranchise/src/db/QStrings.java
+++ JCGFranchise/src/db/QStrings.java
@@ -10,7 +10,7 @@ package db;
  */
 final class QStrings {
     
-    // Fracnhise
+// Fracnhise
     public String update_fran = "UPDATE Franchise SET Address = ?,"
                     + " City = ?, State = ?, Zip = ?, Phone = ?, Email = ?, "
                     + " AirportID = ? WHERE FranchiseNumber = ?";
@@ -18,31 +18,32 @@ final class QStrings {
                     + " State, Zip, Phone, Email, AirportID) values "
                     + "(?, ?, ?, ?, ?, ?, ?)";
     
-    // Vehicle
+// Vehicle
     public String insert_vehicle = "INSERT INTO Vehicle (VehicleID, VIN, Make "
              + "Model, Year, Millage, Capacity, VCondition, Tablet, RentalPrice"
              + " FranchiceNumber VALUES(?,?,?,?,?,?,?,?,?,?,?)";
      
-    public String update_vehicle = "UPDATE Vehicle SET Millage = ?, VCondition "
-             + "= ?, Tablet = ?, RentalPrice = ?, FranchiseNumber = ? WHERE "
-             + "VehicleID = ?";
+    public String update_vehicle = "UPDATE Vehicle SET VehicleID = ?, VIN = ?,"
+            + " Make = ?, Model = ?, Year = ?, Millage = ?, Capacity = ?, "
+            + "VCondition = ?, Tablet = ?, RentalPrice = ?, FranchiseNumber = ? WHERE "
+             + "VIndex = ?";
     
-    // Maintenance
+// Maintenance
     public String insert_maintenance = "INSERT INTO Maintenance(ServiceNumber, "
             + "VehicleID, ServiceType, ServiceCost) VALUES(?,?,?,?)";
     
     public String update_maintenance = "UPDATE Maintenance SET VehicleID = ?, "
-            + "ServiceType = ?, ServiceCost = ? WHERE ServiceNumber = ?";
+            + "ServiceType = ?, ServiceCost = ?, ServiceNumber = ? WHERE MaintIndex = ?";
     
-    // Employee
+// Employee
     public String insert_employee = "INSERT INTO Employee(Fname, Surname,"
             + " Address, City, State, Zip, Phone, Email, EmpType, FranchiseNumber, "
             + "Username) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
     
-    public String insert_customer = "INSERT INTO Customer(Fname, Surname,"
-            + " Address, City, State, Zip, Phone, Email, Username "
-            + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    
+    public String update_employee = "UPDATE Employee SET Fname = ?, Surname = ? "
+            + "Address = ?, City = ?, State = ?, Zip = ?, Phone = ?, Email = ? "
+            + "WHERE EmployeeID = ?";
+            
     public String create_user = "CREATE USER ?@'localhost' IDENTIFIED BY ?";
     
     public String grant_temp = "grant all privileges on *.* to "
@@ -70,23 +71,36 @@ final class QStrings {
     public String grant_owner = "GRANT INSERT, UPDATE, DELETE, SELECT ON JCGroup.Airport TO ?@'localhost' "
                               + "GRANT INSERT, UPDATE, DELETE, SELECT ON JCGroup.Franchise TO ?@'localhost' "
                               + "GRANT INSERT, UPDATE, DELETE, SELECT ON JCGroup.Employee TO ?@'localhost' "
-                              + "GRANT SELECT, UPDATE ON JCGroup.Membership TO ?@'localhost'";
-    
-    public String grant_customer = "GRANT INSERT, UPDATE, DELETE, SELECT ON JCGroup.Customer TO ?@'localhost' "
-                                 + "GRANT SELECT ON JCGroup.Vehicle TO ?@'localhost'";
+                              + "GRANT SELECT, UPDATE ON JCGroup.Membership TO ?@'localhost'";   
     
     public String get_username = "SELECT Username FROM Employee "
-                    + "WHERE Employee.EmpType = ?";
+                    + "WHERE Employee.EmpType = ?";    
     
+// Customer
+    public String insert_customer = "INSERT INTO Customer(Fname, Surname,"
+            + " Address, City, State, Zip, Phone, Email, Username "
+            + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    
+    public String update_customer = "UPDATE Customer SET Fname = ?, Surname = ?, "
+            + "Adress = ?, City = ?, State = ?, Zip = ?, Phone = ?, "
+            + "Email = ? WHERE CustomerID = ?";
+    
+     public String grant_customer = "GRANT INSERT, UPDATE, DELETE, SELECT ON JCGroup.Customer TO ?@'localhost' "
+                                 + "GRANT SELECT ON JCGroup.Vehicle TO ?@'localhost'";
+    
+// Membership
     public String update_membership = "UPDATE Membership SET Discount = ? "
             + "WHERE MemberID = ?";
     
+// Airport
     public String insert_airport = "INSERT INTO Airport(AirportName, Address, City, State, Zip) "
             + "VALUES (?, ?, ?, ?, ?)";
     
     public String update_airport = "UPDATE Airport SET AirportName = ?, Address = ?, "
             + "City = ?, State = ? Zip = ? WHERE AirportID = ?";
     
+    
+// Admin
     public String get_emp_username = "SELECT Username FROM Employee WHERE EmpType <=> 98";
     public String get_cust_username = "SELECT Username FROM Customer";
     public String drop_user = "DROP USER ?@'localhost'";
